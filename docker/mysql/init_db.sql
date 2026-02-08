ccae81529317a8497e6c4773c24058a58714e22b
Freddie Rick Labuanan <frl082998@gmail.com>
2026-02-09 01:18:50 +0800

    chore: add .env file for custom configuration

--- docker/mysql/init_db.sql
+++ docker/mysql/init_db.sql
@@ -1,5 +1,5 @@
--- DROP USER 'xampp'@'%';
-CREATE USER 'xampp'@'%' IDENTIFIED WITH mysql_native_password BY 'Abc@12345';
-ALTER USER 'xampp'@'%' IDENTIFIED WITH mysql_native_password BY 'Abc@12345';
-GRANT ALL PRIVILEGES ON *.* TO 'xampp'@'%';
+-- DROP USER 'developer'@'%';
+CREATE USER 'developer'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+ALTER USER 'developer'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'developer'@'%';
 FLUSH PRIVILEGES;
